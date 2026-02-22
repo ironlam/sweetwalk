ea2ba5816f7ff457af636df9d5dfbf908d6dd4a9
Lamine Diaby <lamine.diaby@incenteev.com>
2026-02-22 22:50:06 +0100

    Scaffold Next.js frontend with Tailwind

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-screen w-screen relative">
+      <div className="absolute inset-0 bg-slate-100 flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-slate-800">Sweet Walk</h1>
+      </div>
+    </main>
+  );
+}
